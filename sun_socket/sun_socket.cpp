40367de2f7ba8a169f6e411d4d357862c5ea3234
鹏程 <2500404063@qq.com>
2018-06-20 22:17:25 +0800

    Update sun_socket.cpp
    修正一些问题，新增连接函数

--- sun_socket/sun_socket.cpp
+++ sun_socket/sun_socket.cpp
@@ -50,7 +50,7 @@ Sun_SOCKET sun_socket::sun_accept(Sun_SOCKET obj, sockaddr_in *eip)
 	return temp;
 }
 
-int sun_socket::sun_send(Sun_SOCKET obj, char * data, int length)
+int sun_socket::sun_send(Sun_SOCKET obj, const char *data, int length)
 {
 	return send(obj, data, length, 0);
 }
@@ -58,18 +58,6 @@ int sun_socket::sun_send(Sun_SOCKET obj, char * data, int length)
 int sun_socket::sun_recv(Sun_SOCKET obj, char * buffer, int length)
 {
 	return recv(obj, buffer, length, 0);
-}
-
-void sun_socket::sun_send_async(Sun_SOCKET obj, char * data, int length)
-{
-	std::thread th_temp(&sun_socket::sun_send, this, obj, data, length);
-	th_temp.detach();
-}
-
-void sun_socket::sun_recv_async(Sun_SOCKET obj, char * buffer, int length)
-{
-	std::thread th_temp(&sun_socket::sun_recv, this, obj, buffer, length);
-	th_temp.detach();
 }
 
 int sun_socket::sun_stop(Sun_SOCKET obj)
@@ -89,6 +77,26 @@ int sun_socket::sun_stop_hard(Sun_SOCKET obj)
 #endif // _MSC_VER
 #if defined (__GNUC__) || defined(__GNUG__)
 	return close(obj);
+#endif // defined
+}
+
+int sun_socket::sun_connect(Sun_SOCKET &sock, const char* ip, unsigned int port)
+{
+#ifdef _MSC_VER
+	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	sockaddr_in eip;
+	eip.sin_family = AF_INET;
+	eip.sin_port = htons(port);
+	inet_pton(AF_INET, ip, &eip.sin_addr.S_un.S_addr);
+	return connect(sock, (sockaddr*)&eip, sizeof(eip));
+#endif // _MSC_VER
+#if defined (__GNUC__) || defined(__GNUG__)
+	sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	sockaddr_in eip;
+	eip.sin_family = PF_INET;
+	eip.sin_port = htons(port);
+	eip.sin_addr.s_addr = inet_addr(ip);
+	return connect(sock, (sockaddr*)&eip, sizeof(eip));
 #endif // defined
 }
 
